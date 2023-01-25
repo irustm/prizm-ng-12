cc35829ff586c860abd65815ca585c85832ef826
irustm <jamakrus@gmail.com>
2023-01-25 19:51:55 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# prizm-ng-12
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/base-angular-12-app-czdwtw)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'component-overview';
+  version = '@1.0.0-beta.21';
+}
